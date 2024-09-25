546b08591f714e9c45699a041bf3f2a5e10d0c26
Tamir <t0548048331@gmail.com>
2024-09-25 16:46:19 +0300

    test

--- selenium_test/webDriver/screp.py
+++ selenium_test/webDriver/screp.py
@@ -1,21 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
-#remove windows warning messages
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-#change path to where your have it!
+
 se = Service(executable_path='selenium_test/webDriver/chromedriver')
-driver = webdriver.Chrome(service=se,options=options)
-driver.get("https://www.shopsite.com/ready-to-buy.html")
+driver = webdriver.Chrome(service=se, options=options)
+driver.get("file:///Users/tamirlevi/DevWithgit/ProjectDev/memory.html")
 
 
-free_btn = driver.find_elements(By.CLASS_NAME,"btn")
-print(free_btn)
-free_btn[1].click()
+
+cards = driver.find_elements(By.CLASS_NAME, "card")
+cardslen = len(cards)
+if len(cards) < 2:
+    raise Exception("Game is not initialized")
+
+# scure check
+for i in range(len(cards)):
+    cards[i].click()
+    for j in range(i+1,len(cards)):  
+     #time.sleep(0.5) 
+     cards[j].click()
+     score_element = driver.find_element(By.ID, "score1") 
+     new_score = score_element.text
+     previous_score = 0
+     try:
+       assert int (new_score) > previous_score # score is not updated need to be fixed < to >
+       previous_score = int(new_score)
+     except AssertionError:
+       print("score is not updated")
+       driver.quit()
+       exit(1)
+
+#restart button check 
+# cards[0].click()
+# time.sleep(1)
+# cards[1].click()
+restars_button = driver.find_element(By.CLASS_NAME, "restartbutton").click()
+time.sleep(0.4)
+try:
+    assert cardslen == len(cards)
+except AssertionError:
+    print("restart button is not working")
+    driver.quit()
+    exit(1)
+   
 
 driver.quit()
-
-
